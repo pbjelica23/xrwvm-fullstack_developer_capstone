d71beb963f6697ba13d6f384e01d1bc402538ab9
Petar Bjelica <petar.bjelica@igamingplatform.com>
2024-09-17 09:50:58 +0200

    Change files

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -16,6 +16,7 @@ sentiment_analyzer_url = os.getenv(
     'sentiment_analyzer_url', default="http://localhost:5050/"
 )
 
+
 def get_request(endpoint, **kwargs):
     """Send a GET request to the specified endpoint with optional query parameters."""
     if kwargs:
@@ -28,13 +29,13 @@ def get_request(endpoint, **kwargs):
     try:
         response = requests.get(request_url)
         response.raise_for_status()  # Raise an exception for HTTP errors
-
         return response.json()
     except requests.exceptions.HTTPError as http_err:
         logger.error(f"HTTP error occurred: {http_err}")
     except requests.exceptions.RequestException as req_err:
         logger.error(f"Network error occurred: {req_err}")
     return None
+
 
 def analyze_review_sentiments(text):
     """Analyze the sentiment of a review using the sentiment analyzer service."""
@@ -42,7 +43,6 @@ def analyze_review_sentiments(text):
     try:
         response = requests.get(request_url)
         response.raise_for_status()  # Raise an exception for HTTP errors
-
         return response.json()
     except requests.exceptions.HTTPError as http_err:
         logger.error(f"HTTP error occurred: {http_err}")
@@ -50,13 +50,13 @@ def analyze_review_sentiments(text):
         logger.error(f"Network error occurred: {req_err}")
     return None
 
+
 def post_review(data_dict):
     """Post a review to the backend service."""
     request_url = f"{backend_url}/insert_review"
     try:
         response = requests.post(request_url, json=data_dict)
         response.raise_for_status()  # Raise an exception for HTTP errors
-
         return response.json()
     except requests.exceptions.HTTPError as http_err:
         logger.error(f"HTTP error occurred: {http_err}")
